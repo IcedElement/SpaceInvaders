bfaa30613d345faa375b39810de7272ecce7bb93
theofan <theogames@outlook.be>
2014-09-24 16:08:33 +0200

    Exceptions

---
+++ Exceptions.java
@@ -0,0 +1,8 @@
+// exceptions 
+
+// parent class for all our exceptions
+class FinanceException extends Exception {
+    public String toString() {
+        return "Unknown exception occured";
+    }
+}
